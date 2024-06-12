b0c05db519c2f5a0952a84174419dc239680feb6
clh2000 <129511861+clh2000@users.noreply.github.com>
2024-06-12 21:21:12 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   appid: "wx879fff8eea0ead95",
   appsecret: "92991d9320d312084bd20ac6316e91f0",
-  template_id: "_BVz4cEcn8hzy5aUAV9wj9WUMfoDCyYSb6Qfzwe8ehQ",
+  template_id: "BVz4cEcn8hzy5aUAV9wj9WUMfoDCyYSb6Qfzwe8ehQ",
   touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
 
   key: "11d1ca4e2da11100fde46aa9cd53cdec",
